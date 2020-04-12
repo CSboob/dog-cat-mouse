6174d7787058c78a41556207a1323b45020b38d6
CalerSchill <tamthieuxz>
2020-04-12 20:36:16 +0700

    add jerry to stomach of tom

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var Dog = require('./Dog');
 var Cat = require('./Cat');
-
+var Mouse = require('./Mouse');
 
 
 var dog = new Dog('Jack');
 var tom = new Cat('Tom');
+var jerry = new Mouse('jerry');
 
 dog.chase(tom);
 
-// console.log(dog);
+tom.eat(jerry)
+
+ console.log(dog);
